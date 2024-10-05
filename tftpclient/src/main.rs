e1f96c79b4007629bd9fa48cc56d16cddd7073f6
Tony Rafanomezantsoa <tony.mandresy@gmail.com>
2024-10-05 13:24:42 +0300

    Update TFTP client

--- tftpclient/src/main.rs
+++ tftpclient/src/main.rs
@@ -1,46 +1,54 @@
 use std::{fmt::format, net::UdpSocket, process, time::Duration};
 
 use rand::Rng;
-use utils::{ClientAction, ClientArgs};
+use utils::{ClientAction, ClientArgs, DataPacket};
 
 mod utils;
 
-fn main() {
-    let client_args = match ClientArgs::build() {
-        Ok(client_args) => client_args,
-        Err(e) => {
-            eprintln!("{}", e);
-            process::exit(1);
-        }
-    };
+fn main() -> Result<(), String> {
+    let client_args = ClientArgs::build()?;
 
     let client_port: u16 = rand::thread_rng().gen_range(0..=65535);
 
-    let client_socket = match UdpSocket::bind(format!("0.0.0.0:{}", client_port)) {
-        Ok(socket) => socket,
-        Err(e) => {
-            eprintln!("Error: Unable to initialize UDP socket client: {}", e);
-            process::exit(1);
-        }
-    };
+    let client_socket = UdpSocket::bind(format!("0.0.0.0:{}", client_port)).map_err(|e| {
+        format!(
+            "Unable to initialize an UDP socket client, please retry: {}",
+            e
+        )
+    })?;
 
     match client_args.action {
         ClientAction::Read => {
-            let server_tid: u16;
+            let read_request =
+                utils::build_read_request_packet(&client_args.filename, &client_args.mode)?;
 
-            let mut read_request: Vec<u8> = Vec::new();
-            // Read request (RRQ) opcode = 1
-            read_request.extend_from_slice(&1_u16.to_be_bytes());
-            // Read request (RRQ) filename
-            read_request.extend_from_slice(client_args.filename.as_bytes());
-            // Read request (RRQ) null byte
-            read_request.push(0);
-            // Read request (RRQ) mode
-            read_request.extend_from_slice(client_args.mode.as_bytes());
-            // Read request (RRQ) null byte
-            read_request.push(0);
+            client_socket
+                .send_to(&read_request, format!("{}:69", client_args.remote_ip))
+                .map_err(|e| format!("Unable to initialize a RRQ: {}", e))?;
 
+            // Create a buffer for a TFTP data packet
+            // The packet structure includes:
+            // - Opcode: 2 bytes (indicates the packet type)
+            // - Block number: 2 bytes (identifies the data block)
+            // - Data: 512 bytes (the actual data being transmitted)
+            let mut response = [0_u8; 516];
+
+            let (_, remote_addr) = client_socket
+                .recv_from(&mut response)
+                .map_err(|e| format!("Unable to receive data packet: {}", e))?;
+
+            let first_data_packet = match DataPacket::parse(&response) {
+                Ok(packet) => packet,
+                // Send an "ERR" packet to the remote server
+                Err(_) => todo!(),
+            };
+
+            if first_data_packet.block != 1 {
+                // Send an "ERR" packet to the remote server
+            }
         }
         ClientAction::Write => {}
     }
+
+    Ok(())
 }
--- tftpclient/src/utils/mod.rs
+++ tftpclient/src/utils/mod.rs
@@ -1,4 +1,4 @@
-use std::{env, net::IpAddr};
+use std::{env, error::Error, net::IpAddr};
 
 /// Represents the actions that a TFTP client can perform.
 ///
@@ -29,7 +29,7 @@ impl ClientArgs {
         let args = env::args().collect::<Vec<String>>();
 
         if args.len() != 5 {
-            return Err(String::from("Error: Invalid arguments"));
+            return Err(String::from("Invalid arguments"));
         }
 
         let action;
@@ -37,20 +37,20 @@ impl ClientArgs {
         match args[1].to_lowercase().as_str() {
             "write" => action = ClientAction::Write,
             "read" => action = ClientAction::Read,
-            _ => return Err(String::from("Error: Invalid [ACTION]")),
+            _ => return Err(String::from("Invalid [ACTION]")),
         }
 
         if args[2].is_empty() {
-            return Err(String::from("Error: [FILENAME] is empty"));
+            return Err(String::from("[FILENAME] is empty"));
         }
 
         let remote_ip = match args[3].parse::<IpAddr>() {
             Ok(ip) => ip,
-            Err(_) => return Err(String::from("Error: Invalid [REMOTE IP ADDRESS]")),
+            Err(_) => return Err(String::from("Invalid [REMOTE IP ADDRESS]")),
         };
 
         if args[4].is_empty() {
-            return Err(String::from("Error: [MODE] is empty"));
+            return Err(String::from("[MODE] is empty"));
         }
 
         Ok(Self {
@@ -59,5 +59,62 @@ impl ClientArgs {
             remote_ip,
             mode: args[4].clone(),
         })
+    }
+}
+
+/// Constructs a Read Request (RRQ) packet for the
+/// Trivial File Transfer Protocol (TFTP) in byte format.
+pub fn build_read_request_packet(filename: &str, mode: &str) -> Result<Vec<u8>, String> {
+    if filename.is_empty() || mode.is_empty() {
+        return Err(String::from(
+            "Unable to build RRQ packet, filename or mode is empty",
+        ));
+    }
+
+    let mut packet: Vec<u8> = Vec::new();
+
+    // RRQ opcode = 1
+    packet.extend_from_slice(&1_u16.to_be_bytes());
+
+    // RRQ filename
+    packet.extend_from_slice(filename.as_bytes());
+
+    // RRQ null byte
+    packet.push(0);
+
+    // RRQ mode
+    packet.extend_from_slice(mode.as_bytes());
+
+    // RRQ null byte
+    packet.push(0);
+
+    Ok(packet)
+}
+
+/// Represents a TFTP Data Packet.
+pub struct DataPacket {
+    pub opcode: u16,
+    pub block: u16,
+    pub data: [u8; 512],
+}
+
+impl DataPacket {
+    /// Parses a byte slice into a `DataPacket`.
+    ///
+    /// This function takes a byte array of length 516, which includes
+    /// the opcode (2 bytes), block number (2 bytes), and data (512 bytes).
+    /// It extracts these components and constructs a `DataPacket` instance.
+    pub fn parse(data: &[u8; 516]) -> Result<Self, String> {
+        let opcode = u16::from_be_bytes(data[0..2].try_into().unwrap());
+        
+        if opcode != 3 {
+            return Err(String::from("Invalid TFTP data packet"));
+        }
+
+        let block = u16::from_be_bytes(data[2..4].try_into().unwrap());
+
+        let data: [u8; 512] = data[4..].try_into().unwrap();
+
+        Ok(Self { opcode, block, data })
     }
 }
