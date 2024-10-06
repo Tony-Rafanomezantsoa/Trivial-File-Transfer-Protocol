396d549f5f408ff677c249d57a41082fa5a99060
Tony Rafanomezantsoa <tony.mandresy@gmail.com>
2024-10-06 17:26:05 +0300

    Update TFTP Client

--- tftpclient/src/main.rs
+++ tftpclient/src/main.rs
@@ -1,17 +1,17 @@
-use std::{fmt::format, net::UdpSocket, process, time::Duration};
+use std::net::UdpSocket;
 
 use rand::Rng;
+use tftppacket::{DATAPacket, ERRORPacket, RRQPacket};
 use utils::{ClientAction, ClientArgs};
-use tftppacket::{DataPacket, RRQPacket};
 
 mod utils;
 
 fn main() -> Result<(), String> {
     let client_args = ClientArgs::build()?;
 
-    let client_port: u16 = rand::thread_rng().gen_range(0..=65535);
+    let client_tid: u16 = rand::thread_rng().gen_range(0..=65535);
 
-    let client_socket = UdpSocket::bind(format!("0.0.0.0:{}", client_port)).map_err(|e| {
+    let client_socket = UdpSocket::bind(format!("0.0.0.0:{}", client_tid)).map_err(|e| {
         format!(
             "Unable to initialize an UDP socket client, please retry: {}",
             e
@@ -20,34 +20,37 @@ fn main() -> Result<(), String> {
 
     match client_args.action {
         ClientAction::Read => {
-            let rrq = RRQPacket::create_rrq_packet(&client_args.filename, &client_args.mode)?;
+            let rrq = RRQPacket::create_rrq_packet(&client_args.filename, &client_args.mode);
 
             client_socket
                 .send_to(&rrq, format!("{}:69", client_args.remote_ip))
                 .map_err(|e| format!("Unable to initialize a RRQ: {}", e))?;
 
-            // Create a buffer for a TFTP data packet
-            // The packet structure includes:
-            // - Opcode: 2 bytes (indicates the packet type)
-            // - Block number: 2 bytes (identifies the data block)
-            // - Data: 512 bytes (the actual data being transmitted)
+            // Create a buffer for the first TFTP DATA packet
+            // - Opcode: 2 bytes
+            // - Block number: 2 bytes
+            // - Data: 512 bytes
             let mut response = [0_u8; 516];
 
             let (_, remote_addr) = client_socket
                 .recv_from(&mut response)
-                .map_err(|e| format!("Unable to receive data packet: {}", e))?;
+                .map_err(|e| format!("Unable to receive the first DATA packet: {}", e))?;
 
-            let first_data_packet = match DataPacket::parse(&response) {
+            let first_data_packet = match DATAPacket::parse(response) {
                 Ok(packet) => packet,
-                // INVALID DATA PACKET
-                // Send an "ERR" packet to the remote server and exit the program
-                Err(e) => todo!(),
+                Err(e) => {
+                    let error = ERRORPacket::create_custom_error_packet(&e);
+                    client_socket.send_to(&error, remote_addr.clone());
+                    return Err(format!("File transmission aborted due to an error: {}", e));
+                }
             };
 
             if first_data_packet.block != 1 {
-                // INVALID DATA PACKET
-                // Send an "ERR" packet to the remote server and exit the program
-                todo!()
+                let error = ERRORPacket::create_custom_error_packet(
+                    "Received data with an invalid block number.",
+                );
+                client_socket.send_to(&error, remote_addr.clone());
+                return Err(String::from("File transmission aborted due to an error: Received data with an invalid block number."));
             }
 
             let server_tid = remote_addr.port();
--- tftppacket/src/lib.rs
+++ tftppacket/src/lib.rs
@@ -1,22 +1,22 @@
 /// Represents a TFTP DATA Packet.
-pub struct DataPacket {
+pub struct DATAPacket {
     pub block: u16,
     pub data: [u8; 512],
 }
 
-impl DataPacket {
+impl DATAPacket {
     pub const OPCODE: u16 = 3;
 
-    /// Parses a byte slice into a `DataPacket`.
+    /// Parses a byte array into a `DATAPacket`.
     ///
     /// This function takes a byte array of length 516, which includes
     /// the opcode (2 bytes), block number (2 bytes), and data (512 bytes).
-    /// It extracts these components and constructs a `DataPacket` instance.
-    pub fn parse(data: &[u8; 516]) -> Result<Self, String> {
+    /// It extracts these components and constructs a `DATAPacket` instance.
+    pub fn parse(data: [u8; 516]) -> Result<Self, String> {
         let opcode = u16::from_be_bytes(data[0..2].try_into().unwrap());
         
         if opcode != Self::OPCODE {
-            return Err(String::from("Invalid TFTP data packet"));
+            return Err(String::from("Invalid TFTP DATA packet"));
         }
 
         let block = u16::from_be_bytes(data[2..4].try_into().unwrap());
@@ -36,15 +36,9 @@ pub struct RRQPacket {
 impl RRQPacket {
     pub const OPCODE: u16 = 1;
 
-    /// Constructs a RRQ packet in byte format
+    /// Constructs a TFTP RRQ packet in byte format
     /// using the specified filename and mode.
-    pub fn create_rrq_packet(filename: &str, mode: &str) -> Result<Vec<u8>, String> {
-        if filename.is_empty() || mode.is_empty() {
-            return Err(String::from(
-                "Unable to build RRQ packet, filename or mode is empty",
-            ));
-        }
-
+    pub fn create_rrq_packet(filename: &str, mode: &str) -> Vec<u8> {
         let mut packet: Vec<u8> = Vec::new();
 
         // RRQ opcode = 1
@@ -62,6 +56,33 @@ impl RRQPacket {
         // RRQ null byte
         packet.push(0);
 
-        Ok(packet)
+        packet
+    }
+}
+
+/// Represents a TFTP ERROR Packet.
+pub enum ERRORPacket {}
+
+impl ERRORPacket {
+    pub const OPCODE: u16 = 5;
+
+    /// Constructs a custom TFTP ERROR packet in byte format
+    /// with ErrorCode = 0 and using the specified error message.
+    pub fn create_custom_error_packet(error_message: &str) -> Vec<u8> {
+        let mut packet: Vec<u8> = Vec::new();
+
+        // ERROR opcode = 5
+        packet.extend_from_slice(&Self::OPCODE.to_be_bytes());
+
+        // Custom ErrCode = 0
+        packet.extend_from_slice(&[0, 0]);
+
+        // ErrorMessage
+        packet.extend_from_slice(error_message.as_bytes());
+        
+        // ERROR null byte
+        packet.push(0);
+
+        packet
     }
 }
