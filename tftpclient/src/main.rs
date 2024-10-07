c2dffcc46b6440777c6156dfa51242d18b85984e
Tony Rafanomezantsoa <tony.mandresy@gmail.com>
2024-10-07 17:50:09 +0300

    Update TFTP client

--- tftpclient/src/main.rs
+++ tftpclient/src/main.rs
@@ -1,12 +1,12 @@
 use std::net::UdpSocket;
 
 use rand::Rng;
-use tftppacket::{DATAPacket, ERRORPacket, RRQPacket};
+use tftppacket::{ACKPacket, DATAPacket, ERRORPacket, RRQPacket, WRQPacket};
 use utils::{ClientAction, ClientArgs};
 
 mod utils;
 
-fn main() -> Result<(), String> {
+fn main() -> Result<(), String> {    
     let client_args = ClientArgs::build()?;
 
     let client_tid: u16 = rand::thread_rng().gen_range(0..=65535);
@@ -55,7 +55,42 @@ fn main() -> Result<(), String> {
 
             let server_tid = remote_addr.port();
         }
-        ClientAction::Write => {}
+        
+        ClientAction::Write => {
+            let wrq = WRQPacket::create_wrq_packet(&client_args.filename, &client_args.mode);
+
+            client_socket
+                .send_to(&wrq, format!("{}:69", client_args.remote_ip))
+                .map_err(|e| format!("Unable to initialize a WRQ: {}", e))?;
+
+            // Create a buffer for the first TFTP ACK packet
+            // - Opcode: 2 bytes
+            // - Block number: 2 bytes
+            let mut response = [0_u8; 4];
+
+            let (_, remote_addr) = client_socket
+                .recv_from(&mut response)
+                .map_err(|e| format!("Unable to receive the first ACK packet: {}", e))?;
+
+            let ack_packet_for_write = match ACKPacket::parse(response) {
+                Ok(packet) => packet,
+                Err(e) => {
+                    let error = ERRORPacket::create_custom_error_packet(&e);
+                    client_socket.send_to(&error, remote_addr.clone());
+                    return Err(format!("File transmission aborted due to an error: {}", e));
+                }
+            };
+
+            if ack_packet_for_write.block != 0 {
+                let error = ERRORPacket::create_custom_error_packet(
+                    "Received ACK with an invalid block number.",
+                );
+                client_socket.send_to(&error, remote_addr.clone());
+                return Err(String::from("File transmission aborted due to an error: Received ACK with an invalid block number."));
+            }
+
+            let server_tid = remote_addr.port();
+        }
     }
 
     Ok(())
--- tftppacket/src/lib.rs
+++ tftppacket/src/lib.rs
@@ -1,32 +1,3 @@
-/// Represents a TFTP DATA Packet.
-pub struct DATAPacket {
-    pub block: u16,
-    pub data: [u8; 512],
-}
-
-impl DATAPacket {
-    pub const OPCODE: u16 = 3;
-
-    /// Parses a byte array into a `DATAPacket`.
-    ///
-    /// This function takes a byte array of length 516, which includes
-    /// the opcode (2 bytes), block number (2 bytes), and data (512 bytes).
-    /// It extracts these components and constructs a `DATAPacket` instance.
-    pub fn parse(data: [u8; 516]) -> Result<Self, String> {
-        let opcode = u16::from_be_bytes(data[0..2].try_into().unwrap());
-        
-        if opcode != Self::OPCODE {
-            return Err(String::from("Invalid TFTP DATA packet"));
-        }
-
-        let block = u16::from_be_bytes(data[2..4].try_into().unwrap());
-
-        let data: [u8; 512] = data[4..].try_into().unwrap();
-
-        Ok(Self { block, data })
-    }
-}
-
 /// Represents a TFTP RRQ Packet.
 pub struct RRQPacket {
     pub filename: String,
@@ -57,6 +28,94 @@ impl RRQPacket {
         packet.push(0);
 
         packet
+    }
+}
+
+/// Represents a TFTP WRQ Packet.
+pub struct WRQPacket {
+    pub filename: String,
+    pub mode: String,
+}
+
+impl WRQPacket {
+    pub const OPCODE: u16 = 2;
+
+    /// Constructs a TFTP WRQ packet in byte format
+    /// using the specified filename and mode.
+    pub fn create_wrq_packet(filename: &str, mode: &str) -> Vec<u8> {
+        let mut packet: Vec<u8> = Vec::new();
+
+        // WRQ opcode = 1
+        packet.extend_from_slice(&Self::OPCODE.to_be_bytes());
+
+        // WRQ filename
+        packet.extend_from_slice(filename.as_bytes());
+
+        // WRQ null byte
+        packet.push(0);
+
+        // WRQ mode
+        packet.extend_from_slice(mode.as_bytes());
+
+        // WRQ null byte
+        packet.push(0);
+
+        packet
+    }
+}
+
+/// Represents a TFTP DATA Packet.
+pub struct DATAPacket {
+    pub block: u16,
+    pub data: [u8; 512],
+}
+
+impl DATAPacket {
+    pub const OPCODE: u16 = 3;
+
+    /// Parses a byte array into a `DATAPacket`.
+    ///
+    /// This function takes a byte array of length 516, which includes
+    /// the opcode (2 bytes), block number (2 bytes), and data (512 bytes).
+    /// It extracts these components and constructs a `DATAPacket` instance.
+    pub fn parse(data: [u8; 516]) -> Result<Self, String> {
+        let opcode = u16::from_be_bytes(data[0..2].try_into().unwrap());
+        
+        if opcode != Self::OPCODE {
+            return Err(String::from("Invalid TFTP DATA packet"));
+        }
+
+        let block = u16::from_be_bytes(data[2..4].try_into().unwrap());
+
+        let data: [u8; 512] = data[4..].try_into().unwrap();
+
+        Ok(Self { block, data })
+    }
+}
+
+/// Represents a TFTP ACK Packet.
+pub struct ACKPacket {
+    pub block: u16,
+}
+
+impl ACKPacket {
+    pub const OPCODE: u16 = 4;
+
+    /// Parses a byte array into a `ACKPacket`.
+    ///
+    /// This function takes a byte array of length 4, which includes
+    /// the opcode (2 bytes) and block number (2 bytes).
+    /// It extracts these components and constructs a `ACKPacket` instance.
+    pub fn parse(data: [u8; 4]) -> Result<Self, String> {
+        let opcode = u16::from_be_bytes(data[0..2].try_into().unwrap());
+
+        if opcode != Self::OPCODE {
+            return Err(String::from("Invalid TFTP ACK packet"));
+        }
+        
+        let block = u16::from_be_bytes(data[2..4].try_into().unwrap());
+
+        Ok(Self { block })
     }
 }
 
